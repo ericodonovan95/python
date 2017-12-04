1e23e978bf5b3fd1923c3f287e59fcf9929f4aae
Eric O Shea <ericodonovan95@gmail.com>
2017-12-04 22:27:44 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 
 from random import randint
+from pip._vendor.distlib.compat import raw_input
 checker = True
 print("Name of the game is dice kid try to get a 6 good luck")
 number = (randint(1, 6))
@@ -13,8 +14,12 @@ while checker:
         checker = False
     
     else:
-        print("unlucky lets roll again")
-        number = (randint(1, 6))
+        second_guess = raw_input("unlucky would you like to roll again yes or no?")
+        if second_guess == "yes":
+            number = (randint(1, 6))
+        
+        else:
+            checker = False
         
 
     
